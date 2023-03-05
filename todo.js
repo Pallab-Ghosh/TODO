cf021d79271cdf3528ae53c5bf75d08ab6bef13e
PALLAB GHOSH <gpallab405@gmail.com>
2023-03-05 17:58:20 +0530

    Add a checked symbol on activities

--- todo.js
+++ todo.js
@@ -20,3 +20,11 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   },false)
 }
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+});
